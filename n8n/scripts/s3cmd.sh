49104779bb23eebdbf909480eb0340d2edf9146f
michelgirault <michel.girault98@gmail.com>
2024-07-29 15:51:18 +0200

    update

--- n8n/scripts/s3cmd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -a
-source .env
-set +a
-
-s3cmd sync --exclude '.git/*' ${MODEL_LOCAL_FOLDER} ${S3_MODEL_BASE} --host=${S3_HOST} --host-bucket=${S3_BUCKET} --secret_key=${S3_SECRET_KEY} --access_key=${S3_ACCESS_KEY}
