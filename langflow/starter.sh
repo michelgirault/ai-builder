fb62d142eeba57714f53118a483ee7a825ab3b52
michelgirault <michel.girault98@gmail.com>
2024-08-21 09:40:04 +0200

    Update starter.sh

--- langflow/starter.sh
+++ langflow/starter.sh
@@ -2,5 +2,5 @@
 #custom script for ai model workflow builder
 
 chroma run --host localhost --port 8000 --path ./my_chroma_data &
-python3 -m langflow run
+python3 -m langflow run --components-path /app/components
 wait
