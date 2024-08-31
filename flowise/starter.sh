214225d5f0ea25f70aab861caa7aa52fe1d0933b
michelgirault <michel.girault98@gmail.com>
2024-08-31 08:44:18 +0200

    add aws

--- flowise/starter.sh
+++ flowise/starter.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #custom script for ai model workflow builder
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $aws_secret_access_key
 
 flowise start
 wait
