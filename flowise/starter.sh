d78117be6ef8ab893ba536864b6f1f94862cc2b8
michelgirault <michel.girault98@gmail.com>
2024-09-01 09:35:48 +0200

    add chroma
    add chroma

--- flowise/starter.sh
+++ flowise/starter.sh
@@ -3,5 +3,6 @@
 aws configure set aws_access_key_id $aws_access_key_id
 aws configure set aws_secret_access_key $aws_secret_access_key
 
+chroma run --host localhost --port 3333 --path /data/chroma &
 flowise start
 wait
