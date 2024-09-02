c53b939525a8124f6d2aecc72dc5133fd9196410
michelgirault <michel.girault98@gmail.com>
2024-09-02 18:01:47 +0200

    Update starter.sh

--- langflow/starter.sh
+++ langflow/starter.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #custom script for ai model workflow builder
 
+aws configure set aws_access_key_id $aws_access_key_id
+aws configure set aws_secret_access_key $aws_secret_access_key
+
 if [ -d $LANGFLOW_DATASETS_DIR ]; then
     echo "Directory exists."
 else
