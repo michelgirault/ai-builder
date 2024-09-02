636647ac2da37eb6f45c902ebe197e5d7ad2b287
michelgirault <michel.girault98@gmail.com>
2024-09-02 14:40:43 +0200

    Update starter.sh

--- langflow/starter.sh
+++ langflow/starter.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 #custom script for ai model workflow builder
 
+if [ -d $LANGFLOW_DATASETS_DIR ]; then
+    echo "Directory exists."
+else
+    mkdir $LANGFLOW_DATASETS_DIR 
+fi
+
+
 aws s3 sync --endpoint-url=$S3_ENDPOINT $LANGFLOW_DATASETS_DIR $S3_BUCKET
 
 chroma run --host localhost --port 8000 --path /app/chrome &
