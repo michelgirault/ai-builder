869d1f0b5c7c8a03540628b82114f0bbeef6e5d1
michelgirault <michel.girault98@gmail.com>
2024-09-02 10:39:21 +0200

    add
    add

--- langflow/starter.sh
+++ langflow/starter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #custom script for ai model workflow builder
 
-chroma run --host localhost --port 8000 --path ./my_chroma_data &
-python3 -m langflow run --components-path /app/components
+chroma run --host localhost --port 8000 --path /app/chrome &
+python3 -m langflow run 
 wait
