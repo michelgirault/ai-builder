20bccd7bc9ab26e0b8f8926696e3131156db05ea
michelgirault <michel.girault98@gmail.com>
2024-09-02 11:14:43 +0200

    add
    add

--- langflow/starter.sh
+++ langflow/starter.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #custom script for ai model workflow builder
 
+aws s3 sync --endpoint-url=$S3_ENDPOINT $LANGFLOW_DATASETS_DIR $S3_BUCKET
+
 chroma run --host localhost --port 8000 --path /app/chrome &
 python3 -m langflow run 
 wait
