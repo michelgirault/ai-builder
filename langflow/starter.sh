56c674709e2f937917670f21da804208822cc113
michelgirault <michel.girault98@gmail.com>
2024-09-02 17:34:24 +0200

    Update starter.sh

--- langflow/starter.sh
+++ langflow/starter.sh
@@ -6,10 +6,11 @@ if [ -d $LANGFLOW_DATASETS_DIR ]; then
 else
     mkdir $LANGFLOW_DATASETS_DIR 
 fi
-
-
+#sync the drive from s3
 aws s3 sync --endpoint-url=$S3_ENDPOINT $LANGFLOW_DATASETS_DIR $S3_BUCKET
+#run the vector db
 
 chroma run --host localhost --port 8000 --path /app/chrome &
+#run the ai builder
 python3 -m langflow run 
 wait
