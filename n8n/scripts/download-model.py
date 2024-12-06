215df310b99b4d04cd1112776251f3fc86dcd973
michelgirault <michel.girault98@gmail.com>
2024-12-06 13:43:24 +0100

    Delete download-model.py

--- n8n/scripts/download-model.py
+++
@@ -1,9 +0,0 @@
-from huggingface_hub import hf_hub_download
-import joblib
-
-REPO_ID = "michelgi/LLaVA-proto"
-FILENAME = "sklearn_model.joblib"
-
-model = joblib.load(
-    hf_hub_download(repo_id=REPO_ID, filename=FILENAME)
-)
